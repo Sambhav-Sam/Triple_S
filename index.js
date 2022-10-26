82cc897d3e5d8daca0bb1e64963119d9ae526b08
Sambhav Jain <sambhavjain233@gmail.com>
2022-10-26 11:19:33 +0530

    connection to database

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//requiring external pakages
+require("dotenv").config();
 const express = require("express");
 
+//requiring internal dependencies
+require("./src/db/connection");
 
 const app = express();
 app.use(express.static("public"));
@@ -10,6 +14,8 @@ app.get("/login",(req,res)=>{
     res.render("login_registeration/login.ejs");
 });
 
+
+//port
 const PORT = 3000;
 app.listen(PORT,(req,res)=>{
     console.log(`server started on port:${PORT}`);
