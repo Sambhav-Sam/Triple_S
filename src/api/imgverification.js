711f5d1899d5d1fbb7c46019253afc570cd66537
Sambhav Jain <sambhavjain233@gmail.com>
2022-11-08 12:51:28 +0530

    live pic now can be sended to server

--- src/api/imgverification.js
+++ src/api/imgverification.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const isAuth = require("../../routes/auth/isauth");
-const axios = require('axios');
-const UserDetail = require("../models/userDetails");
+const upload = require("express-fileupload");
 
 const router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
     extended: true
 }));
+router.use(upload());
 
 router.post("/", async (req, res) => {
     try {
         //authenticating user
         const user = await isAuth(req);
         
-        console.log(req.body);
+        console.log(req.files);
 
     } catch (error) {
         console.log(error);
